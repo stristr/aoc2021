6b67f87d8bd731d5272134d5c3981f9207b086b1
Sasha Joseph <sjoseph@netflix.com>
2021-12-06 21:25:27 -0800

    Day 7

---
+++ 07.py
@@ -0,0 +1,9 @@
+# ?/?
+# ?:??/?:??
+from sys import stdin
+from statistics import median
+
+nums = list(sorted(list(map(int, stdin.read().strip().split(',')))))
+m = int(median(nums))
+print('a', sum(abs(m - n) for n in nums))
+print('b', min(sum(abs(x - n) * (abs(x - n) + 1) // 2 for n in nums) for x in range(min(nums), max(nums))))
